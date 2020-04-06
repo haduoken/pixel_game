b98637d1b217125bab6e10ca3fe13695a231e5a3
yijie <zhangyj@kilox.cn>
2020-04-06 23:54:01 +0800

    add more

--- examples/01_hello_pyxel.py
+++ examples/01_hello_pyxel.py
@@ -4,7 +4,7 @@ import pyxel
 class App:
     def __init__(self):
         pyxel.init(160, 120, caption="Hello Pyxel")
-        # pyxel.image(0).load(0, 0, "assets/pyxel_logo_38x16.png")
+        pyxel.image(0).load(0, 0, "assets/pyxel_logo_38x16.png")
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -15,6 +15,7 @@ class App:
         pyxel.cls(0)
         # 颜色变化的字体, 这个里面最多16色
         pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
+        # 将xy处将iamge0的左上角+wh 描绘上去
         pyxel.blt(61, 66, 0, 0, 0, 38, 16)
 
 
