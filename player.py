9fcc6b54e4f5dccb406d7f72aada191c21b54b49
yijie <zhangyj@kilox.cn>
2020-04-07 22:36:13 +0800

    add more

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import pyxel
+from math import sqrt
 
 
 class Player:
@@ -6,17 +7,44 @@ class Player:
         self.position = [0, 0]
         self.animation_type = 'stand'
         self.player_animation = {}
+        self.speed = 1
         pass
-    
-    def move(self, speed_x, speed_y):
+
+    def move(self, direction):
+        speed_x, speed_y = 0, 0
+        if direction == 'up':
+            speed_y = -self.speed
+        elif direction == 'down':
+            speed_y = self.speed
+        elif direction == 'left':
+            speed_x = -self.speed
+        elif direction == 'right':
+            speed_x = self.speed
+
+        elif direction == 'up-left':
+
+            speed_x = -self.speed * sqrt(2) / 2.0
+            speed_y = -self.speed * sqrt(2) / 2.0
+
+        elif direction == 'up-right':
+            speed_x = self.speed * sqrt(2) / 2.0
+            speed_y = -self.speed * sqrt(2) / 2.0
+        elif direction == 'down-left':
+            speed_x = -self.speed * sqrt(2) / 2.0
+            speed_y = self.speed * sqrt(2) / 2.0
+        elif direction == 'down-right':
+            speed_x = self.speed * sqrt(2) / 2.0
+            speed_y = self.speed * sqrt(2) / 2.0
+
         self.position[0] += speed_x
         self.position[1] += speed_y
-        pass
-    
+
     def act(self, type):
+
         self.type = type
-    
+
     def update(self):
+
         # image = self.player_animation[self.animation_type].get_next_frame()
         pyxel.blt(
             self.position[0],
--- scene.py
+++ scene.py
@@ -2,41 +2,64 @@ import pyxel
 from player import Player
 from pyxel import Image
 
-a = Image()
-
 
 class App:
     def __init__(self):
         pyxel.init(160, 120, caption="Hello Pyxel")
         pyxel.load("assets/jump_game.pyxres")
-        
+
         # pyxel.image(0).load(0, 0, "assets/pyxel_logo_38x16.png")
         self.p1 = Player('abc')
         self.p2 = Player('abc')
+        self.cnt = 0
         pyxel.run(self.update, self.draw)
-    
+
     def update(self):
+        self.cnt += 1
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
-        
-        # 两个player的移动
+
+        if pyxel.btn(pyxel.KEY_A) :
+            print('key A', self.cnt)
         if pyxel.btn(pyxel.KEY_W):
-            self.p1.move(0, -1)
-        elif pyxel.btn(pyxel.KEY_S):
-            self.p1.move(0, 1)
-        elif pyxel.btn(pyxel.KEY_A):
-            self.p1.move(-1, 0)
-        elif pyxel.btn(pyxel.KEY_D):
-            self.p1.move(1, 0)
-        elif pyxel.btn(pyxel.KEY_UP):
-            self.p2.move(0, -1)
-        elif pyxel.btn(pyxel.KEY_DOWN):
-            self.p2.move(0, 1)
-        elif pyxel.btn(pyxel.KEY_LEFT):
-            self.p2.move(-1, 0)
-        elif pyxel.btn(pyxel.KEY_RIGHT):
-            self.p2.move(1, 0)
-    
+            print('hey W', self.cnt)
+        if pyxel.btn(pyxel.KEY_A) and pyxel.btn(pyxel.KEY_W):
+            print('a and w')
+        # 两个player的移动
+        # if pyxel.btnp(pyxel.KEY_A):
+        #     self.p1.move('left-up')
+        # elif pyxel.btn(pyxel.KEY_D) and pyxel.btn(pyxel.KEY_W):
+        #     self.p1.move('right-up')
+        # elif pyxel.btn(pyxel.KEY_A) and pyxel.btn(pyxel.KEY_S):
+        #     self.p1.move('left-down')
+        # elif pyxel.btn(pyxel.KEY_D) and pyxel.btn(pyxel.KEY_S):
+        #     self.p1.move('right-down')
+        # elif pyxel.btn(pyxel.KEY_W):
+        #     self.p1.move('up')
+        # elif pyxel.btn(pyxel.KEY_S):
+        #     self.p1.move('down')
+        # elif pyxel.btn(pyxel.KEY_A):
+        #     self.p1.move('left')
+        # elif pyxel.btn(pyxel.KEY_D):
+        #     self.p1.move('right')
+        #
+        # if pyxel.btn(pyxel.KEY_LEFT) and pyxel.btn(pyxel.KEY_UP):
+        #     self.p2.move('left-up')
+        # elif pyxel.btn(pyxel.KEY_RIGHT) and pyxel.btn(pyxel.KEY_UP):
+        #     self.p2.move('right-up')
+        # elif pyxel.btn(pyxel.KEY_LEFT) and pyxel.btn(pyxel.KEY_DOWN):
+        #     self.p2.move('left-down')
+        # elif pyxel.btn(pyxel.KEY_RIGHT) and pyxel.btn(pyxel.KEY_DOWN):
+        #     self.p2.move('right-down')
+        # elif pyxel.btn(pyxel.KEY_UP):
+        #     self.p2.move('up')
+        # elif pyxel.btn(pyxel.KEY_DOWN):
+        #     self.p2.move('down')
+        # elif pyxel.btn(pyxel.KEY_LEFT):
+        #     self.p2.move('left')
+        # elif pyxel.btn(pyxel.KEY_RIGHT):
+        #     self.p2.move('right')
+
     def draw(self):
         pyxel.cls(0)
         self.p1.update()
